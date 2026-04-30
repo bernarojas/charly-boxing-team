2edb4bd0b5f83129edb0236d5e0284f66be5ff8b
bernarojas <242213256+bbernarojas@users.noreply.github.com>
2026-04-30 03:18:20 -0400

    Quinto commit: Se cambian las imagenes

--- app/page.tsx
+++ app/page.tsx
@@ -18,8 +18,8 @@ export default function HomePage() {
       {/* HERO */}
       <section className="flex flex-col items-center text-center gap-6">
         <Image
-          src="/CBT_fotografia.png"
-          //src="/CBT_dorado.png"
+          //src="/CBT_fotografia.png"
+          src="/CBT_dorado.png"
           alt="Charly Boxing Team"
           width={500}
           height={500}
